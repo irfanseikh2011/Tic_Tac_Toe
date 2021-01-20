9911d734898bb455bb493202366d82a8b568a55f
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-01-20 20:44:47 +0530

    Added Sass for styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Board from './components/Board';
+import "./styles/root.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <h1>Tic Tac Toe Game</h1>
       <Board/>
     </div>
   );
