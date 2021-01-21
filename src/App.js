fa4779146e778c050d89ade40eb92533c6f010ef
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-01-21 22:13:12 +0530

    Added Logic for Calculating Winner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
 import Board from './components/Board';
 import "./styles/root.scss";
+import React, {useState} from 'react';
+import {calculateWinner} from './helper';
 
-function App() {
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner ? `Winner is ${winner}` : `Next Player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+
+  const WinnerHighlight = () => {
+    if(winner)
+    {
+    
+    }
+  }
+
   return (
     <div className="app">
       <h1>Tic Tac Toe Game</h1>
-      <Board/>
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick}/>
     </div>
   );
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Square = (props) => {
-    return (
-        
-            <button type="button" className="square">{props.value}</button>
-    )
-}
+const Square = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
 
 export default Square;
