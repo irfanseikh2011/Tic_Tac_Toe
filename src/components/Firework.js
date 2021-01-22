227475d00aa065a3c77b47a05136299668e87ff0
irfanseikh2011 <seikhirfan2011@gmail.com>
2021-01-22 17:17:51 +0530

    added more fireworks

--- src/components/Firework.js
+++ src/components/Firework.js
@@ -2,13 +2,13 @@ import { Fireworks } from 'fireworks/lib/react'
 
 function Firework(props) {
   let fxProps = {
-    count: 3,
-    interval: 200,
+    count: 5,
+    interval: 1000,
     colors: ['#12E177', '#FFC72A', '#FFC72A'],
     calc: (props, i) => ({
       ...props,
-      x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
-      y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+      x: (i + 1) * (window.innerWidth / 3.5 ) - (i + 1) * 100,
+      y: 350 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
     })
   }
 
